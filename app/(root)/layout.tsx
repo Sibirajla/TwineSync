d4db86be91c7a1175ef6879082f2ff9401ade9cb
SIBIRAJ L A <sibirajl007@gmail.com>
2023-11-14 10:54:23 +0530

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
+  title: "TwineSync",
   description: "A Next.js 13 Meta Threads application",
 };
 
