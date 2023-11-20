44eaad9095c9ae9ddad862416775938d1a70e9f4
SIBIRAJ L A <115621077+Sibirajla@users.noreply.github.com>
2023-11-20 11:45:13 +0530

    index.js

--- constants/index.js
+++ constants/index.js
@@ -17,7 +17,7 @@ export const sidebarLinks = [
   {
     imgURL: "/assets/create.svg",
     route: "/create-thread",
-    label: "Create Thread",
+    label: "Create Twine",
   },
   {
     imgURL: "/assets/community.svg",
