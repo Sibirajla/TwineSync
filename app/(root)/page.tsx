adfd4eab626e03e4c5bf40a509819ae3cd80c9e2
SIBIRAJ L A <115621077+Sibirajla@users.noreply.github.com>
2023-11-20 12:02:06 +0530

    page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,7 +29,7 @@ async function Home({
 
       <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
-          <p className='no-result'>No threads found</p>
+          <p className='no-result'>No twines found</p>
         ) : (
           <>
             {result.posts.map((post) => (
